c3eb03492bc6f742f66d36bf10531bfcab5f6a53
Murilo Portescheller <mport.dev@gmail.com>
2018-01-30 11:04:38 -0200

    componentes criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CartaoComponent } from './cartao/cartao.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartaoComponent
   ],
   imports: [
     BrowserModule
